374696268e79cdf0a9e6a702cf6e3aa12b0c83c3
QuentinSpace <pro.quentint@gmail.com>
2024-04-03 23:32:35 +0200

    tru

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/:path*',
+				has: [{ type: 'host', value: 'www.mooya.fr' }],
+				destination: 'https://mooya.fr/:path*',
+				permanent: true,
+			},
+			// If you need more redirects, they can be added JS objects here.
+		];
+	},
+};
 
 export default nextConfig;
