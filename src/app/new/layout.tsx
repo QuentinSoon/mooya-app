6bced197c7a10d7e694b3e297e1e258a65fe780a
QuentinSpace <pro.quentint@gmail.com>
2024-05-01 14:48:28 +0200

    update

--- src/app/new/layout.tsx
+++ src/app/new/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
-	title: 'Espace locataire - Mooya',
+	title: 'Nouveaux logements - Mooya',
 	description:
 		'Mooya est une plateforme de location et gestion de biens immobiliers nouvelle génération. Trouvez votre prochain logement en quelques clics !',
 };
@@ -18,13 +18,5 @@ export default async function HostingLayout({
 	if (error || !data?.user) {
 		redirect('/');
 	}
-	return (
-		<div className="relative h-screen w-screen grid grid-cols-2">
-			<div></div>
-			<div className="flex flex-col justify-between bg-red-200">
-				<div className="border-b h-20"></div>
-				<div className="grow"></div>
-			</div>
-		</div>
-	);
+	return <section>{children}</section>;
 }
--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,11 @@
 export default async function Page() {
-	return <>tt</>;
+	return (
+		<div className="relative h-screen w-screen grid grid-cols-2">
+			<div></div>
+			<div className="flex flex-col justify-between bg-red-200">
+				<div className="border-b h-20"></div>
+				<div className="grow"></div>
+			</div>
+		</div>
+	);
 }
