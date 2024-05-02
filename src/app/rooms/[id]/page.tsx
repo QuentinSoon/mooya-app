646cecbfdc5c3f75aca74cd1d9ca9ea699571f57
QuentinSpace <pro.quentint@gmail.com>
2024-05-02 21:35:21 +0200

    add checker

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -70,7 +70,7 @@ export default async function Page({ params }: Props) {
 					</div>
 					<div>
 						<h1 className="font-semibold text-xl text-left text-[--color-basic-900]">
-							{rooms.title}
+							{rooms?.title}
 						</h1>
 						<div className="font-normal text-sm text-left text-[--color-basic-600]">
 							{rooms.structure.title} - {rooms.privacy.title} -{' '}
@@ -234,23 +234,24 @@ export default async function Page({ params }: Props) {
 				<SectionRooms title="Ce que propose ce logement">
 					<div className="space-y-6">
 						<div className={`grid grid-cols-2 gap-8 w-2/4`}>
-							{rooms.test.map((amenity: any, index: number) => (
-								<div
-									key={index}
-									className="flex flex-row space-x-4 justify-start items-center"
-								>
-									<div className="flex flex-col">
-										<Icon
-											name={amenity.amenities.icon_id ?? 'TriangleAlert'}
-											size={24}
-											color="currentColor"
-										/>
+							{rooms.test &&
+								rooms.test.map((amenity: any, index: number) => (
+									<div
+										key={index}
+										className="flex flex-row space-x-4 justify-start items-center"
+									>
+										<div className="flex flex-col">
+											<Icon
+												name={amenity.amenities.icon_id ?? 'TriangleAlert'}
+												size={24}
+												color="currentColor"
+											/>
+										</div>
+										<div className="flex flex-col space-y-0.5 w-full text-base font-medium text-[--color-basic-900] text-left">
+											{amenity.amenities.title ?? 'Error'}
+										</div>
 									</div>
-									<div className="flex flex-col space-y-0.5 w-full text-base font-medium text-[--color-basic-900] text-left">
-										{amenity.amenities.title ?? 'Error'}
-									</div>
-								</div>
-							))}
+								))}
 						</div>
 					</div>
 					<Button size="md" className="w-fit" variant="outline">
